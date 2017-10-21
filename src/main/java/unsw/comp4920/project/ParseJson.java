2115e491936750db26b8808c480e2ad72af80a50
DARREN-COMP\Darren <hzkayyt@gmail.com>
2017-10-21 15:45:33 +1100

    testing out databasing the json

---
+++ src/main/java/unsw/comp4920/project/ParseJson.java
@@ -0,0 +1,49 @@
+package unsw.comp4920.project;
+
+import com.github.wnameless.json.flattener.JsonFlattener;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.stream.JsonReader;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.Map;
+
+public class ParseJson {
+
+
+    public static void main(String [ ] args) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader("recipe_test.json"));
+        for (String line = br.readLine(); line != null; line = br.readLine()) {
+            System.out.println(line);
+            //Map<String,Object> flattened = JsonFlattener.flattenAsMap(line);
+            //System.out.println(flattened);
+            JsonElement root = new JsonParser().parse(line);
+
+            for (JsonElement e : root.getAsJsonArray()){
+                System.out.println(e);
+                System.out.println(e.getAsJsonObject().get("_id").getAsJsonObject().get("$oid"));
+                System.out.println(e.getAsJsonObject().get("name"));
+                System.out.println(e.getAsJsonObject().get("ingredients"));
+                System.out.println(e.getAsJsonObject().get("ingredients"));
+            }
+        }
+
+        /*
+        Type REVIEW_TYPE = new TypeToken<List<Review>>() {}.getType();
+        Gson gson = new Gson();
+        JsonReader reader = null;
+        try {
+            reader = new JsonReader(new FileReader("recipe_test.json"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        List<Review> data = gson.fromJson(reader, REVIEW_TYPE); // contains the whole reviews list
+        data.toScreen();
+        */
+    }
+}
